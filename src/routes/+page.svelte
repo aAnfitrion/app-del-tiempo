85428e61ed4103baa7266eaeaf364d82b6048f4f
aAnfitrion <tendondevastador@hotmail.com>
2023-06-07 09:40:44 -0500

    Añadimos el fetch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script>
+  const options = {
+    method: 'GET',
+    headers: {
+	  	'X-RapidAPI-Key': 'ff01acc76cmshf428cc9ea4499f5p1755e1jsn24fd3095aa53',
+		  'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+	  }
+  }
+
+  fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13', options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err))
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
